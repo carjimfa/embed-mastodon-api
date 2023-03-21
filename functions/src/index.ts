ce2431e6d559e0ad8fd835f92bc5476bb9d48786
Carlos Draper Giggs <carjimfa@gmail.com>
2023-03-21 11:16:51 +0100

    Adds first functional version

---
+++ functions/src/index.ts
@@ -0,0 +1,34 @@
+import * as functions from "firebase-functions";
+import * as path from "path";
+import axios, {AxiosResponse} from "axios";
+import * as express from "express";
+import * as cors from "cors";
+const app = express();
+
+app.use(cors());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/:username/:domain", async (req: any, res: any) => {
+  const domain = req.params.domain;
+  const username = req.params.username;
+  const baseUrl = `https://${domain}/api/v1`;
+  const accountUrl = `${baseUrl}/accounts/lookup?acct=${username}`;
+
+  axios.get(accountUrl).then((user: AxiosResponse<any>) => {
+    axios.get(`${baseUrl}/accounts/${user.data.id}/statuses`)
+        .then((statuses: AxiosResponse<any>) => {
+          res.render("index", {
+            username: user.data.username,
+            statuses: statuses.data,
+          });
+        });
+  });
+});
+
+app.get("/", (req: any, res: any) => {
+  res.send("hooray, it works in index");
+});
+
+exports.app = functions.https.onRequest(app);
